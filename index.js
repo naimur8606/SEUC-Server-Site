33cfab07823dc1eb62852ec00abc5a616e197c29
naimur8606 <119111473+naimur8606@users.noreply.github.com>
2024-01-06 22:53:48 +0600

    user add and display API created

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require("cors");
+const jwt = require('jsonwebtoken');
 require('dotenv').config()
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
@@ -7,6 +8,7 @@ const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.kc6dmnx.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -22,9 +24,30 @@ const client = new MongoClient(uri, {
 
 async function run() {
     try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
+
+        const allUsers = client.db("SEU-Communication").collection("Users");
+
+
+        app.get("/Users", async (req, res) => {
+            const result = await allUsers.find().toArray();
+            res.send(result);
+        })
+
+        app.post("/Users", async (req, res) => {
+            const user = req?.body;
+            const query = { email: req?.body?.email };
+            const present = await allUsers.findOne(query)
+            if (!present) {
+                const result = await allUsers.insertOne(user);
+                res.send(result);
+            }
+        })
+
         // Send a ping to confirm a successful connection
-        // await client.db("admin").command({ ping: 1 });
-        // console.log("Pinged your deployment. You successfully connected to MongoDB!");
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
         // Ensures that the client will close when you finish/error
         // await client.close();
